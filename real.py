ec3159c2c980ea0d91583895bb9fe485ae4d81e6
Samar Ibrahim <43992889+bioengsamar@users.noreply.github.com>
2021-05-09 00:28:35 +0200

    Add files via upload

--- real.py
+++ real.py
@@ -4,8 +4,12 @@ import struct
 import time
 import librosa
 import numpy as np
+from matplotlib import cm
+import pylab
+import cv2
+import librosa.display
 
-#cough_model = load_model("vgg16_model_15_frozen_layers.json")
+#cough_model = load_model("cough_detector.model")
 
 # constants
 CHUNK = 55125            # samples per frame
@@ -41,16 +45,30 @@ while True:
     data_int = struct.unpack(str(2 * CHUNK) + 'B', data)
     
     # create np array and offset by 128
-    data_np = (np.array(data_int, dtype='b')).astype('float64')
-    print(len(data_np))
+    data_np = (np.array(data_int, dtype='b')).astype('float32')
+    length = data_np.shape[0] / RATE
+    #print(length)
     # Compute spectrogram
-   # M = librosa.feature.melspectrogram(data_np, RATE, 
-                                       #fmax = RATE/2, # Maximum frequency to be used on the on the MEL scale
-                                       #n_fft=2048, 
-                                       #hop_length=512, 
-                                       #n_mels = 96, # As per the Google Large-scale audio CNN paper
-                                       #power = 2) # Power = 2 refers to squared amplitude
+    M = librosa.feature.melspectrogram(data_np, RATE, 
+                                       fmax = RATE/2, # Maximum frequency to be used on the on the MEL scale
+                                       n_fft=2048, 
+                                       hop_length=512, 
+                                       n_mels = 96, # As per the Google Large-scale audio CNN paper
+                                       power = 2) # Power = 2 refers to squared amplitude
     
-    #print(M)
-   
+    #print(M.shape)
+    frame_count += 1
+    log_power = librosa.power_to_db(M, ref=np.max)# Covert to dB (log) scale
+    # # Plotting the spectrogram and save as JPG without axes (just the image)
+    pylab.figure(figsize=(3,3))
+    pylab.axis('off') 
+    pylab.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[]) # Remove the white edge
+    librosa.display.specshow(log_power, cmap=cm.jet)
+    pylab.savefig('record_'+str(frame_count)+ '_.jpg', bbox_inches=None, pad_inches=0)
+    pylab.close()
+    img_array = cv2.imread('record_'+str(frame_count)+ '_.jpg')  # convert to array
+    new_array = cv2.resize(img_array, (224, 224))  # resize to normalize data size
+    new_array=np.array(new_array).reshape(-1, 224, 224, 3)
+    new_array = np.array(new_array, dtype="float32")
+    print(new_array.shape)
     
